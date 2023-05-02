54b4dceb67e6aae60155152c122b43e4050f9ece
Bruno Abitbol <babitbol@equalexperts.com>
2023-05-02 15:44:13 +0100

    updated code to allow writes access

--- authorize-endpoint/server.py
+++ authorize-endpoint/server.py
@@ -3,8 +3,9 @@ import string
 import pkce
 import random
 import aiohttp
+
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 
 app = FastAPI()
 states = {}
@@ -17,8 +18,8 @@ token_url = "https://login.microsoftonline.com/organizations/oauth2/v2.0/token"
 async def authorize_url(client_id: str,
                         aad=authorize_url,
                         response_type="code",
-                        scope="openid",
-                        redirect_uri="http://localhost:8000"):
+                        scope="openid https://positdev.sharepoint.com/AllSites.Write",
+                        redirect_uri="https://denodo.positdev.co.uk"):
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(32))
     code_verifier, code_challenge = pkce.generate_pkce_pair()
     states[state] = {"code_verifier": code_verifier, "client_id": client_id, "redirect_uri": redirect_uri}
@@ -40,8 +41,10 @@ async def challenge():
 async def callback(code: str, state: str):
     if state not in states:
         raise HTTPException(status_code=400, detail="invalid state")
-
     state_value = states[state]
+
+    del states[state]
+
     payload = {
         "grant_type": "authorization_code",
         "code": code,
@@ -50,9 +53,36 @@ async def callback(code: str, state: str):
         "code_verifier": state_value["code_verifier"]
     }
 
+    def html(token, access_token=""):
+        return f"""
+         <html>
+         <head>
+         <title>Denodo access</title>
+         </head>
+         <body>
+             <h2>Denodo ODBC connection</h2>
+             <p>Copy paste the following code snippet to create your Denodo ODBC connection in R studio</p>
+             <textarea cols="120" rows="20">
+                library(DBI)
+                con <- dbConnect(odbc::odbc(), "Denodo_Oauth_DSN",
+                UseOAuth2 = 1,
+                AccessToken = "{token}", 
+                timeout = 10)
+                
+                sharePointAccessToken="{access_token}"
+             </textarea>
+         </body>
+         </html>
+        """
+
     async with aiohttp.ClientSession() as session:
         async with session.post(token_url, data=payload) as resp:
             if resp.ok:
-                return await resp.json()
+                tkn_json = await resp.json()
+                id_token = tkn_json["id_token"]
+                sp_access_token = tkn_json["access_token"]
+                return HTMLResponse(content=html(id_token,
+                                                 access_token=sp_access_token))
             else:
+                print(resp)
                 raise HTTPException(status_code=400, detail="bad request")
